cbc74eac7db1ce0d905942ee956806b902af420e
frepingod <frepingod@gmail.com>
2021-10-29 21:47:44 +0300

    changed list to map

--- src/main/java/ru/netology/repository/UserRepository.java
+++ src/main/java/ru/netology/repository/UserRepository.java
@@ -6,24 +6,28 @@ import ru.netology.model.User;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static ru.netology.authorities.Authorities.*;
 
 @Repository
 public class UserRepository {
 
-    private List<User> users = Arrays.asList(
-            new User("login1", "pass1", Arrays.asList(READ, WRITE, DELETE)),
-            new User("login2", "pass2", Arrays.asList(READ, WRITE)),
-            new User("login3", "pass3", List.of(DELETE))
-    );
+    private Map<String, User> users = new ConcurrentHashMap<>();
+
+    {
+        User user1 = new User("login1", "pass1", Arrays.asList(READ, WRITE, DELETE));
+        User user2 = new User("login2", "pass2", Arrays.asList(READ, WRITE));
+        User user3 = new User("login3", "pass3", List.of(DELETE));
+
+        users.put(user1.getLogin(), user1);
+        users.put(user2.getLogin(), user2);
+        users.put(user3.getLogin(), user3);
+    }
 
     public List<Authorities> getUserAuthorities(String login, String password) {
-        for (User user : users) {
-            if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
-                return user.getAuthorities();
-            }
-        }
-        return null;
+        User user = users.get(login);
+        return (user != null && user.getPassword().equals(password)) ? user.getAuthorities() : null;
     }
 }
